84b36b7eca4594238c502bd12fdc56379da945fc
Ojas <ojasat5@gmail.com>
2024-06-07 19:33:39 +0530

    added trap routines, mamory mappers and finished the lc3 VM

--- src/main.cc
+++ src/main.cc
@@ -56,6 +56,24 @@ enum
     FL_NEG = 1 << 2,
 };
 
+// Trap codes
+enum
+{
+    TRAP_GETC = 0x20,  /* get character from keyboard, not echoed onto the terminal */
+    TRAP_OUT = 0x21,   /* output a character */
+    TRAP_PUTS = 0x22,  /* output a word string */
+    TRAP_IN = 0x23,    /* get character from keyboard, echoed onto the terminal */
+    TRAP_PUTSP = 0x24, /* output a byte string */
+    TRAP_HALT = 0x25   /* halt the program */
+};
+
+// memory mapped registers
+enum
+{
+    MR_KBSR = 0xFE00, /* keyboard status */
+    MR_KBDR = 0xFE02  /* keyboard data */
+};
+
 uint16_t sign_extend(uint16_t x, int bit_count)
 {
     if ((x >> (bit_count - 1)) & 1)
@@ -63,6 +81,19 @@ uint16_t sign_extend(uint16_t x, int bit_count)
         x |= (0xFFFF << bit_count);
     }
     return x;
+}
+
+HANDLE hStdin = INVALID_HANDLE_VALUE;
+DWORD fdwMode, fdwOldMode;
+
+uint16_t check_key()
+{
+    return WaitForSingleObject(hStdin, 1000) == WAIT_OBJECT_0 && _kbhit();
+}
+
+uint16_t swap16(uint16_t x)
+{
+    return (x << 8) | (x >> 8);
 }
 
 void update_flags(uint16_t r)
@@ -79,6 +110,82 @@ void update_flags(uint16_t r)
     {
         reg[R_COND] = FL_POS;
     }
+}
+
+void read_image_file(FILE *file)
+{
+    uint16_t origin;
+    fread(&origin, sizeof(origin), 1, file);
+    origin = swap16(origin);
+
+    uint16_t max_read = MEMORY_MAX - origin;
+    uint16_t *p = memory + origin;
+    size_t read = fread(p, sizeof(uint16_t), max_read, file);
+
+    while (read-- > 0)
+    {
+        *p = swap16(*p);
+        ++p;
+    }
+}
+
+int read_image(const char *image_path)
+{
+    FILE *file = fopen(image_path, "rb");
+    if (!file)
+    {
+        return 0;
+    };
+    read_image_file(file);
+    fclose(file);
+    return 1;
+}
+
+void mem_write(uint16_t address, uint16_t val)
+{
+    memory[address] = val;
+}
+
+uint16_t mem_read(uint16_t address)
+{
+    if (address == MR_KBSR)
+    {
+        if (check_key())
+        {
+            memory[MR_KBSR] = (1 << 15);
+            memory[MR_KBDR] = getchar();
+        }
+        else
+        {
+            memory[MR_KBSR] = 0;
+        }
+    }
+    return memory[address];
+}
+
+// Input Buffering
+
+void disable_input_buffering()
+{
+    hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    GetConsoleMode(hStdin, &fdwOldMode);     /* save old mode */
+    fdwMode = fdwOldMode ^ ENABLE_ECHO_INPUT /* no input echo */
+              ^ ENABLE_LINE_INPUT;           /* return when one or
+                                                more characters are available */
+    SetConsoleMode(hStdin, fdwMode);         /* set new mode */
+    FlushConsoleInputBuffer(hStdin);         /* clear buffer */
+}
+
+void restore_input_buffering()
+{
+    SetConsoleMode(hStdin, fdwOldMode);
+}
+
+void handle_interrupt(int signal)
+{
+    restore_input_buffering();
+    printf("\n");
+    exit(-2);
 }
 
 int main(int argc, const char *argv[])
@@ -99,6 +206,8 @@ int main(int argc, const char *argv[])
     }
 
     // setup
+    signal(SIGINT, handle_interrupt);
+    disable_input_buffering();
 
     reg[R_COND] = FL_ZRO;
 
@@ -114,7 +223,7 @@ int main(int argc, const char *argv[])
     {
 
         // fetch
-        uint16_t instr = mem_read(reg_PC++);
+        uint16_t instr = mem_read(reg[R_PC]++);
         uint16_t op = instr >> 12;
 
         switch (op)
@@ -265,7 +374,65 @@ int main(int argc, const char *argv[])
         }
         break;
         case OP_TRAP:
-            @{ TRAP } break;
+        {
+            reg[R_R7] = reg[R_PC];
+
+            switch (instr & 0xFF)
+            {
+            case TRAP_GETC:
+            {
+                reg[R_R0] = (uint16_t)getchar();
+                update_flags(R_R0);
+            }
+            break;
+            case TRAP_OUT:
+                break;
+            case TRAP_PUTS:
+            {
+                uint16_t *c = memory + reg[R_R0];
+                while (*c)
+                {
+                    putc((char)*c, stdout);
+                    ++c;
+                }
+                fflush(stdout);
+            }
+            break;
+            case TRAP_IN:
+            {
+                printf("Enter a character: ");
+                char c = getchar();
+                putc(c, stdout);
+                fflush(stdout);
+                reg[R_R0] = (uint16_t)c;
+                update_flags(R_R0);
+            }
+            break;
+            case TRAP_PUTSP:
+            {
+                uint16_t *c = memory + reg[R_R0];
+                while (*c)
+                {
+                    char char1 = (*c) & 0xFF;
+                    putc(char1, stdout);
+                    char char2 = (*c) >> 8;
+                    if (char2)
+                        putc(char2, stdout);
+                    ++c;
+                }
+                fflush(stdout);
+            }
+            break;
+            case TRAP_HALT:
+            {
+                puts("HALT");
+                fflush(stdout);
+                running = 0;
+            }
+            break;
+            }
+        }
+        break;
         case OP_RES:
         case OP_RTI:
         default:
@@ -274,4 +441,5 @@ int main(int argc, const char *argv[])
         }
     };
     // @{ Shutdown }
+    restore_input_buffering();
 }
